3d68e6dab626bdf2089f94db770c1e4c0c64e73b
ggace <skydream5485@gmail.com>
2021-12-31 00:24:28 +0900

    중간 저장

--- index.py
+++ index.py
@@ -1,145 +1,210 @@
 import pygame
 import random
-
-def showImg(img, x,y):
-    screen.blit(img, (x,y))
-
-pygame.init()
-
-pygame.display.set_caption("음식 받기") #<title>
-
-#스크린 구성
-size  = [800,1000]
-screen= pygame.display.set_mode(size)
-
-finish = False
-
-score = 0
-maxScore = score
-
-font = pygame.font.Font('freesansbold.ttf', 20) 
-scoreText = font.render(f'SCORE : {score}', True, (0,0,0), (255,255,255)) 
-scoreTextRect = scoreText.get_rect()  
-scoreTextRect.x = 0
-scoreTextRect.y = 0
-maxscoreText = font.render(f'MAX SCORE : {maxScore}', True, (0,0,0), (255,255,255)) 
-maxscoreTextRect = maxscoreText.get_rect()  
-maxscoreTextRect.x = 0
-maxscoreTextRect.y = 20
-
-
-
-fallingObjectsize = (100, 100)
-pocketObjectsize = (150, 100)
-
-cakeImg = pygame.transform.scale(pygame.image.load('resources\cake.png'), fallingObjectsize)
-cookieImg = pygame.transform.scale(pygame.image.load('resources\cookie.webp'), fallingObjectsize)
-shitImg = pygame.transform.scale(pygame.image.load('resources\shit.png'), fallingObjectsize)
-pocketImg = pygame.transform.scale(pygame.image.load('resources\pocket.png'), pocketObjectsize)
+import values
 
 class Image:
     def __init__(self, img, score, locate):
         self.image = img
         self.score = score
         self.locate = locate
+def showImg(img, x,y):
+    screen.blit(img, (x,y))
+def showImgs(imgs):
+    for img in imgs:
+        showImg(img.image, img.locate[0], img.locate[1])
+def loadImage(path, size):
+    return pygame.transform.scale(pygame.image.load(path), size)
 
-cake = Image(cakeImg, 3, [0,0]) 
-cookie = Image(cookieImg,1, [0,0])
-shit = Image(shitImg, -4, [0,0])
-pocket = Image(pocketImg, 0, [0,size[1] - pocketObjectsize[1]])
+def getTextByTop(text, fontsize, textColor, bgcolor, x, y):
+    font = pygame.font.Font('freesansbold.ttf', fontsize) 
+    text = font.render(text, True, textColor, bgcolor)
+    textRect = text.get_rect()
+    textRect.x = x
+    textRect.y = y
+    return text, textRect
+def getTextByCenter(text, fontsize, textColor, bgcolor, x, y):
+    font = pygame.font.Font('freesansbold.ttf', fontsize) 
+    text = font.render(text, True, textColor, bgcolor)
+    textRect = text.get_rect()
+    textRect.center = (x, y)
+    return text, textRect
 
+
+def movePocket():
+    pressed = pygame.key.get_pressed()
+    if pressed[pygame.K_a]: pocket.locate[0] -= values.pocketSpeed*speed
+    elif pressed[pygame.K_d]: pocket.locate[0] += values.pocketSpeed*speed
+    elif pressed[pygame.K_RIGHT]: pocket.locate[0] += values.pocketSpeed*speed
+    elif pressed[pygame.K_LEFT]: pocket.locate[0] -= values.pocketSpeed*speed
+    
+def resetFallObject():
+    global turn
+    global locate
+    global speed
+    global fallSpeed
+    global objects
+    global objectIndex
+    
+    turn += 1
+    
+    locate[1] = 0-values.fallingObjectsize[1]
+    objectIndex = random.choice(percentages)
+    locate = objects[objectIndex].locate
+    locate[0] = random.randint(0, values.size[0]-values.fallingObjectsize[0])
+    
+    fallSpeed = values.fallSpeed * speed
+    speed += values.addingSpeed
+    
+#시작
+pygame.init()
+
+#title
+pygame.display.set_caption("음식 받기")
+
+#스크린
+screen= pygame.display.set_mode(values.size)
+
+#트리거
+finish = False
+isSetUp = True
+isGameIn = True
+gameStartTrigger = False
+
+#이미지 불러오기
+backgroundImg = loadImage(r'resources\background.jpg', (values.size[0], values.size[1]))
+cakeImg = loadImage(r'resources\cake.png', values.fallingObjectsize)
+cookieImg = loadImage(r'resources\cookie.png', values.fallingObjectsize)
+shitImg = loadImage(r'resources\shit.png', values.fallingObjectsize)
+pocketImg = loadImage(r'resources\pocket.png', values.pocketObjectsize)
+
+#이미지 클래스화(이미지 파일, 점수, 위치)
+cake = Image(cakeImg, values.cakeScore, [random.randint(0, values.size[0]-values.fallingObjectsize[0]),0]) 
+cookie = Image(cookieImg,values.cookieScore, [random.randint(0, values.size[0]-values.fallingObjectsize[0]),0])
+shit = Image(shitImg, values.shitScore, [random.randint(0, values.size[0]-values.fallingObjectsize[0]),0])
+pocket = Image(pocketImg, None, [values.size[0]/2-values.pocketObjectsize[0]/2,values.size[1] - values.pocketObjectsize[1] - 10])
+
+#기본 세팅
+score = 0
+maxScore = score
+turn = 0
 objects = [cake, cookie, shit]
+speed = values.speed
+fallSpeed = values.fallSpeed
 
-cakePercentage = 10
-cookiePercentage = 45
-shitPercentage = 45
+#문자 세팅
+scoreText, scoreTextRect = getTextByTop(f'SCORE : {score}', 20, values.textColor,values.bgcolor, values.smallNotice[0], values.smallNotice[1])
+maxscoreText, maxscoreTextRect = getTextByTop(f'MAX SCORE : {maxScore}', 20, values.textColor,values.bgcolor, values.smallNotice[0], values.smallNotice[1]+20)
+turnText, turnTextRect = getTextByTop(f'TURN : {turn}', 20, values.textColor,values.bgcolor, values.smallNotice[0], values.smallNotice[1]+40)
+maxscoreLargeText, maxscoreLargeTextRect = getTextByCenter(f'MAX SCORE : {maxScore}', 50, values.textColor,values.bgcolor, values.size[0]/2, values.size[1]/2-25)
+turnLargeText, turnLargeTextRect = getTextByCenter(f'TURN : {turn}', 50, values.textColor,values.bgcolor, values.size[0]/2, values.size[1]/2+25)
+restartLargeText, restartLargeTextRect = getTextByCenter(f'PRESS SPACE BAR TO RESTART', 20, values.textColor,values.bgcolor, values.size[0]/2, values.size[1]/2+60)
 
+#확률 작업
 percentages = []
 
-for i in range(cakePercentage):
+for i in range(values.cakePercentage):
     percentages.append(0)
-for i in range(cookiePercentage):
+for i in range(values.cookiePercentage):
     percentages.append(1)
-for i in range(shitPercentage):
+for i in range(values.shitPercentage):
     percentages.append(2)
 
+#object 선택
 objectIndex = random.choice(percentages)
 locate = objects[objectIndex].locate
-speed = 1
 
-fallSpeed = 0.2
-
-resultPercentage = [0,0,0]
-
-
-isGameIn = True
+#게임 시작
 while not(finish):
-    screen.fill((80,100,255))
-    
+    #배경화면
+    showImg(backgroundImg, 0,0)
 
+    #이벤트 처리
     for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                finish = True
-    
+        #종료처리
+        if event.type == pygame.QUIT:
+            finish = True
+        
+            
+    #게임 중
     if(isGameIn):
-    
-        pressed = pygame.key.get_pressed()
+        #시작시 초기화
+        if(isSetUp):
+            score = 0
+            maxScore = score
+            turn = 0
+            speed = values.speed
+            fallSpeed = values.fallSpeed
+            
+            objectIndex = random.choice(percentages)
+            locate = objects[objectIndex].locate
+            
+            cake = Image(cakeImg, values.cakeScore, [random.randint(0, values.size[0]-values.fallingObjectsize[0]),0]) 
+            cookie = Image(cookieImg,values.cookieScore, [random.randint(0, values.size[0]-values.fallingObjectsize[0]),0])
+            shit = Image(shitImg, values.shitScore, [random.randint(0, values.size[0]-values.fallingObjectsize[0]),0])
+            pocket = Image(pocketImg, None, [values.size[0]/2-values.pocketObjectsize[0]/2,values.size[1] - values.pocketObjectsize[1] - 10])  
+        #트리거 처리 
+        isSetUp = False 
         
-        if pressed[pygame.K_a]: pocket.locate[0] -= 1*speed
-        elif pressed[pygame.K_d]: pocket.locate[0] += 1*speed
-        elif pressed[pygame.K_RIGHT]: pocket.locate[0] += 1*speed
-        elif pressed[pygame.K_LEFT]: pocket.locate[0] -= 1*speed
+        #pocket 움직임 처리
+        movePocket()
         
+        #포켓 위치 제한
         if(pocket.locate[0] < 0): pocket.locate[0] = 0
-        elif(pocket.locate[0] > size[0]-pocketObjectsize[0]): pocket.locate[0] = size[0]-pocketObjectsize[0]
+        elif(pocket.locate[0] > values.size[0]-values.pocketObjectsize[0]): pocket.locate[0] = values.size[0]-values.pocketObjectsize[0]
             
         
-        showImg(objects[objectIndex].image, objects[objectIndex].locate[0], objects[objectIndex].locate[1])
-        showImg(pocket.image, pocket.locate[0], pocket.locate[1])
         
-        
-        
-        if(locate[1] >= size[1]-fallingObjectsize[1]):
-            if(locate[0]+fallingObjectsize[0] > pocket.locate[0] and locate[0]-fallingObjectsize[0] < pocket.locate[0] + pocketObjectsize[0] and locate[1] >= size[1]-fallingObjectsize[1] and locate[1] <= size[1]):
+        #받음 처리
+        if(locate[1] >= values.size[1]-values.fallingObjectsize[1]):
+            #받았을때
+            if(locate[0]+values.fallingObjectsize[0] > pocket.locate[0] and locate[0]-values.fallingObjectsize[0] < pocket.locate[0] + values.pocketObjectsize[0] and locate[1] >= values.size[1]-values.fallingObjectsize[1] and locate[1] <= values.size[1]):
                 score += objects[objectIndex].score
                 
                 if(maxScore < score):
                     maxScore = score
+                resetFallObject()
                 
-                locate[1] = 0-fallingObjectsize[1]
-                objectIndex = random.choice(percentages)
-                resultPercentage[objectIndex] += 1
-                locate = objects[objectIndex].locate
-                locate[0] = random.randint(0, size[0]-fallingObjectsize[0])
-                fallSpeed = 0.2 * speed
                 
-                speed += 0.001
+            #못받고 끝에 도달하지 않았을 때
+            elif(locate[1] < 1000):
+                locate[1] += fallSpeed*speed
+                fallSpeed += values.addingFallSpeed * speed
                 
-                print(score)
                 
-            elif(locate[1] < 1200):
-                locate[1] += speed
-                fallSpeed += 0.002 * speed
+            #못받고 끝에 도달했을 때
             else:
+                resetFallObject()
                 
-                locate[1] = 0-fallingObjectsize[1]
-                objectIndex = random.choice(percentages)
-                resultPercentage[objectIndex] += 1
-                locate = objects[objectIndex].locate
-                locate[0] = random.randint(0, size[0]-fallingObjectsize[0])
-                fallSpeed = 0.2 * speed
-                speed += 0.001
-        
         else:
-            locate[1] += speed
-            fallSpeed += 0.002 * speed
-        scoreText = font.render(f'SCORE : {score}', True, (0,0,0), (255,255,255)) 
-        maxscoreText = font.render(f'MAX SCORE : {maxScore}', True, (0,0,0), (255,255,255)) 
+            locate[1] += fallSpeed*speed
+            fallSpeed += values.addingFallSpeed * speed
+            
+        #text처리
+        scoreText, scoreTextRect = getTextByTop(f'SCORE : {score}', 20, values.textColor,values.bgcolor, values.smallNotice[0], values.smallNotice[1])
+        maxscoreText, maxscoreTextRect = getTextByTop(f'MAX SCORE : {maxScore}', 20, values.textColor,values.bgcolor, values.smallNotice[0], values.smallNotice[1]+20)
+        turnText, turnTextRect = getTextByTop(f'TURN : {turn}', 20, values.textColor,values.bgcolor, values.smallNotice[0], values.smallNotice[1]+40)
         screen.blit(scoreText, scoreTextRect) 
         screen.blit(maxscoreText, maxscoreTextRect) 
-        if(score < 0): 
+        screen.blit(turnText, turnTextRect) 
+        #이미지 보여주기
+        showImgs([objects[objectIndex], pocket])
+        #점수가 0이하일 때
+        if(score < 0):  
             isGameIn = False
-    
+            gameStartTrigger = False
+    #게임 오버
+    else:
+        #text처리
+        maxscoreLargeText, maxscoreLargeTextRect = getTextByCenter(f'MAX SCORE : {maxScore}', 50,values.textColor,values.bgcolor, values.size[0]/2, values.size[1]/2-25)
+        turnLargeText, turnLargeTextRect = getTextByCenter(f'TURN : {turn}', 50, values.textColor,values.bgcolor, values.size[0]/2, values.size[1]/2+25)
+        screen.blit(maxscoreLargeText, maxscoreLargeTextRect) 
+        screen.blit(turnLargeText, turnLargeTextRect) 
+        screen.blit(restartLargeText, restartLargeTextRect) 
+        #restart
+        pressed = pygame.key.get_pressed()
+        if pressed[pygame.K_SPACE] and not gameStartTrigger: isGameIn = True; isSetUp = True; gameStartTrigger = True;
+        
+        
     
         
     pygame.display.flip()
---
+++ values.py
@@ -0,0 +1,29 @@
+#속도
+speed = 1
+fallSpeed = 3
+pocketSpeed = 5
+addingSpeed = 0.005
+addingFallSpeed = 0.02
+
+#점수
+cakeScore = 3
+cookieScore = 1
+shitScore = -4
+
+#화면구성
+size  = [800,1000]
+fallingObjectsize = (70, 70)
+pocketObjectsize = (150, 60)
+
+#확률
+cakePercentage = 10
+cookiePercentage = 45
+shitPercentage = 45
+
+#색
+textColor=(0,0,0)
+bgcolor = (255,255,255)
+
+#위치
+smallNotice = (0,0)
+bigNotice = (values.size[0]/2, values.size[1]/2)
